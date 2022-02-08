1626941605ad7fe14f6250853f20389697f8852e
theseus yang <theseusyang@users.noreply.github.com>
2022-02-08 11:08:31 +0800

    Add files via upload

---
+++ fs.h
@@ -0,0 +1,28 @@
+/*******************************************************************************
+ *
+ *  Copyright (c) 2019-2022 TimesGraph
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ ******************************************************************************/
+
+#ifndef FS_H
+#define FS_H
+
+extern "C"
+{
+    void *openShm0(const char *name, size_t len, int64_t *hMapping, int id);
+    jint closeShm0(const char *name, void *mem, size_t len, int64_t hMapping);
+}
+
+#endif //FS_H
