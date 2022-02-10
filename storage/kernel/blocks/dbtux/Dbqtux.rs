0373c86fcba856f1e3b52637df341daddefd501a
theseus yang <theseusyang@users.noreply.github.com>
2022-02-10 21:46:49 +0800

    Add files via upload

--- storage/kernel/blocks/dbtux/Dbqtux.rs
+++ storage/kernel/blocks/dbtux/Dbqtux.rs
@@ -21,6 +21,25 @@ impl Dbqtux for Dbtux
 
   static getTransactionMemoryNeed() -> u64;
 
+  pub fn get_transaction_memory_need() -> u64
+{
+  let query_instance_count: u32 = globalData.ndbMtQueryThreads + globalData.ndbMtRecoverThreads;
+  let mut scan_op_byte_count: u64 = 1;
+  let tux_scan_recs: u32 = 1;
+  let tux_scan_lock_recs: u32 = 1;
+
+  scan_op_byte_count += ScanOp_pool::getMemoryNeed(tux_scan_recs);
+  scan_op_byte_count *= query_instance_count;
+
+  let mut scan_lock_byte_count: u64 = 0;
+  scan_lock_byte_count += ScanLock_pool::getMemoryNeed(tux_scan_lock_recs);
+  scan_lock_byte_count *= query_instance_count;
+
+  const nScanBoundWords: u32 = tux_scan_recs * ScanBoundSegmentSize * 4;
+  let scan_bound_byte_count: u64 = nScanBoundWords * query_instance_count;
+
+  return (scan_op_byte_count + scan_lock_byte_count + scan_bound_byte_count);
+}
   fn BLOCK_DEFINES(Dbqtux);
 };
 
