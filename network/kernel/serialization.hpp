1571f2e98f56b99e0b2c8a195cbcbf56d9a3c945
theseus yang <theseusyang@users.noreply.github.com>
2022-03-03 10:33:10 +0800

    Add files via upload

--- network/kernel/serialization.hpp
+++ network/kernel/serialization.hpp
@@ -24,13 +24,13 @@
 
 namespace slk {
 
-inline void Save(const io::network::Endpoint &endpoint, slk::Builder *builder) {
+inline void Save(const network::kernel::Endpoint &endpoint, slk::Builder *builder) {
   slk::Save(endpoint.address_, builder);
   slk::Save(endpoint.port_, builder);
   slk::Save(endpoint.family_, builder);
 }
 
-inline void Load(io::network::Endpoint *endpoint, slk::Reader *reader) {
+inline void Load(network::kernel::Endpoint *endpoint, slk::Reader *reader) {
   slk::Load(&endpoint->address_, reader);
   slk::Load(&endpoint->port_, reader);
   slk::Load(&endpoint->family_, reader);
