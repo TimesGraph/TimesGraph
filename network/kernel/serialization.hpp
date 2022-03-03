8274f2970c737593045ec6486892a91a9173966b
theseus yang <theseusyang@users.noreply.github.com>
2022-03-03 10:12:41 +0800

    Add files via upload

---
+++ network/kernel/serialization.hpp
@@ -0,0 +1,39 @@
+/*******************************************************************************
+ *
+ *
+ *  Copyright (c) 2019-2022 TimesGraph
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ ******************************************************************************/
+
+#pragma once
+
+#include "network/kernel/endpoint.hpp"
+#include "slk/serialization.hpp"
+
+namespace slk {
+
+inline void Save(const io::network::Endpoint &endpoint, slk::Builder *builder) {
+  slk::Save(endpoint.address_, builder);
+  slk::Save(endpoint.port_, builder);
+  slk::Save(endpoint.family_, builder);
+}
+
+inline void Load(io::network::Endpoint *endpoint, slk::Reader *reader) {
+  slk::Load(&endpoint->address_, reader);
+  slk::Load(&endpoint->port_, reader);
+  slk::Load(&endpoint->family_, reader);
+}
+
+}  // namespace slk
